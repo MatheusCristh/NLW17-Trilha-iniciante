758a641d8a404543bcbfac3f9c9939a04056a0d0
Cristhian Brito <matheuscristhianbritodocarmo@gmail.com>
2024-09-10 20:43:55 -0300

    fundamentos

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+    value: 'ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
