959e921cbbf1a598274b1bdebdd7c10a58d6200a
Cristhian Brito <matheuscristhianbritodocarmo@gmail.com>
2024-09-11 12:54:54 -0300

    menu da aplicação

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const start = () => {
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
     
      while(true){
-       let opcao ='sair'
+       
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+        
+
        switch(opcao){
          case "cadastrar":
             console.log("vamos cadastrar")
@@ -10,6 +31,7 @@ const start = () => {
             console.log("vamos listar")
             break
          case "sair":
+            console.log("Até a próxima")
             return
        }
     }
