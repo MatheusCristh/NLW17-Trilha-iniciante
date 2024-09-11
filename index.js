636e6b80c093dd79d20dd9d3051e7d37dea9affb
Cristhian Brito <matheuscristhianbritodocarmo@gmail.com>
2024-09-11 12:19:19 -0300

    fundamentos while - 002

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const start = () => {
-    let count = 0
-     while(count <= 10){
-        console.log(count)
-        count = count + 1
-     }
+    
+     while(true){
+       let opcao ='sair'
+       switch(opcao){
+         case "cadastrar":
+            console.log("vamos cadastrar")
+            break
+         case "listar":
+            console.log("vamos listar")
+            break
+         case "sair":
+            return
+       }
+    }
 }
 
 start()
