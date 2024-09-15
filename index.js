5f0b125a409e4efc1ac31648550e9984dcc14ed6
Cristhian Brito <matheuscristhianbritodocarmo@gmail.com>
2024-09-15 09:53:19 -0300

    cadastrr meta

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: 'Tomar 3L de agua por dia',
+    checked: false,
+}
+
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input ({ message: "Digite a meta"})
+
+    if(meta.length == 0) {
+        console.log('A meta não pode ser vazia.')
+        return
+
+    }
+
+    metas.push(
+        { value: meta, checked: false }
+    )
+
+}
 
 const start = async () => {
     
@@ -25,7 +48,8 @@ const start = async () => {
 
        switch(opcao){
          case "cadastrar":
-            console.log("vamos cadastrar")
+            await cadastrarMeta()
+            console.log(metas)
             break
          case "listar":
             console.log("vamos listar")
